4f3d4c6de8719504061ad9a530f3f487d557b727
Leo Yao <woshiyao68@gmail.com>
2015-04-10 00:04:38 +0800

    state pattern

--- src/main/java/com/ygq/statepattern/GumballMachine.java
+++ src/main/java/com/ygq/statepattern/GumballMachine.java
@@ -6,7 +6,7 @@ public class GumballMachine {
 	State noQuarterState;
 	State hasQuarterState;
 	State soldState;
-	State state = soldOutState;
+	State state;
 	int count = 0;
 
 	public GumballMachine(int count) {
@@ -15,42 +15,29 @@ public class GumballMachine {
 		this.noQuarterState = new NoQuarterState(this);
 		this.hasQuarterState = new HasQuarterState(this);
 		this.soldState = new SoldState(this);
+		if (count > 0) {
+			state = noQuarterState;
+		} else {
+			state = soldOutState;
+		}
 	}
 	public State getSoldOutState() {
 		return soldOutState;
 	}
-	public void setSoldOutState(State soldOutState) {
-		this.soldOutState = soldOutState;
-	}
 	public State getNoQuarterState() {
 		return noQuarterState;
-	}
-	public void setNoQuarterState(State noQuarterState) {
-		this.noQuarterState = noQuarterState;
 	}
 	public State getHasQuarterState() {
 		return hasQuarterState;
 	}
-	public void setHasQuarterState(State hasQuarterState) {
-		this.hasQuarterState = hasQuarterState;
-	}
 	public State getSoldState() {
 		return soldState;
-	}
-	public void setSoldState(State soldState) {
-		this.soldState = soldState;
-	}
-	public State getState() {
-		return state;
 	}
 	public void setState(State state) {
 		this.state = state;
 	}
 	public int getCount() {
 		return count;
-	}
-	public void setCount(int count) {
-		this.count = count;
 	}
 	public void insertQuarter() {
 		state.insertQuarter();
@@ -60,8 +47,6 @@ public class GumballMachine {
 	}
 	public void turnCrank() {
 		state.turnCrank();
-	}
-	public void dispense() {
 		state.dispense();
 	}
 }
--- src/main/java/com/ygq/statepattern/StateTestDrive.java
+++ src/main/java/com/ygq/statepattern/StateTestDrive.java
@@ -4,5 +4,16 @@ public class StateTestDrive {
 
 	public static void main(String[] args) {
 		GumballMachine gumballMachine = new GumballMachine(5);
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.ejectQuarter();
+		gumballMachine.ejectQuarter();
 	}
 }
